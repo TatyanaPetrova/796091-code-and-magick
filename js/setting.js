684a8076be523f098b769b66cbd14cf91bac22cd
TatyanaPetrova <Petrova_tss@mail.ru>
2018-11-18 23:20:46 +0300

    добавление пробелов

--- js/setting.js
+++ js/setting.js
@@ -1,18 +1,18 @@
 var fireballSize = 22;
-var getFireballSpeed = function(left) {
- return left ? 5 : 2;
+var getFireballSpeed = function (left) {
+  return left ? 5 : 2;
 };
 
 var wizardSpeed = 3;
 var wizardWidth = 70;
-var getWizardHeight = function() {
+var getWizardHeight = function () {
   return 1.337 * wizardWidth;
 };
 
-var getWizardX = function(width) {
- return width/2 - wizardWidth/2;
+var getWizardX = function (width) {
+  return width / 2 - wizardWidth / 2;
 };
 
-var getWizardY = function(height) {
-  return height/3;
+var getWizardY = function (height) {
+  return height / 3;
 };
